f6b336bf84c6f2add11500a547fec6b51ebe414a
Dione-b <dionenassau321@gmail.com>
2024-03-21 17:30:57 -0300

    chore: implements cors policies

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes import router as pdf_router
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # Permite requisições de qualquer lugar (temporário)
+    allow_credentials=True,
+    allow_methods=["*"], # Permite todos os métodos HTTP
+    allow_headers=["*"], # Permite todos os cabeçalhos
+)
 
 app.include_router(pdf_router)
