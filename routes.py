9180935ca8b8e60318125696444c736d1b6534f5
Dione-b <dionenassau321@gmail.com>
2024-03-26 17:56:53 -0300

    feat: add download route pdf

--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter, UploadFile, File, Body, HTTPException
+from fastapi import APIRouter, UploadFile, File, Body, HTTPException, Path
 from services import PDFService
 import os
+from datetime import datetime
 
 router = APIRouter()
 
@@ -30,8 +31,29 @@ async def list_pdfs():
     """ Lista todos os PDF salvos em disco """
     UPLOAD_DIR = "uploads"
     try:
-        pdf_files = [f for f in os.listdir(UPLOAD_DIR) if f.endswith('.pdf')]
-        return {"pdfs": pdf_files}
+        pdf_files_info = []
+        for f in os.listdir(UPLOAD_DIR):
+            if f.endswith('.pdf'):
+                file_path = os.path.join(UPLOAD_DIR, f)
+                modification_time = os.path.getmtime(file_path)
+                modification_date = datetime.fromtimestamp(modification_time)
+                formatted_date = modification_date.strftime('%Y-%m-%d %H:%M:%S')
+                pdf_files_info.append({"title": f, "modification_date": formatted_date})
+        return {"pdfs": pdf_files_info}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.delete("/delete-pdf/{pdf_name}")
+async def delete_pdf(pdf_name: str = Path(...)):
+    """ Exclui um arquivo PDF da pasta uploads """
+    UPLOAD_DIR = "uploads"
+    file_path = os.path.join(UPLOAD_DIR, pdf_name)
+    try:
+        if os.path.exists(file_path) and os.path.isfile(file_path):
+            os.remove(file_path)
+            return {"detail": f"O arquivo {pdf_name} foi excluído com sucesso."}
+        else:
+            raise HTTPException(status_code=404, detail=f"O arquivo {pdf_name} não foi encontrado.")
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
